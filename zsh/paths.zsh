d4c62450df82c8c50e943bdf0381b879675bd2e3
Jonathan Grubb <grubb2k7@gmail.com>
2026-05-04 12:58:00 -0400

    - Adding root project node modules bin folder to path. - Removed redundant ./node_modules/bin path - fixed pnpm bin --global path

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,3 +1,3 @@
-path=(./node_modules/.bin "${path[@]}")
-path+=(pnpm bin --global)
+path+="${PROJECT_ROOT:+$PROJECT_ROOT/node_modules/.bin}"
+path+=$(pnpm bin --global 2> /dev/null)
 path+='/Applications/IntelliJ IDEA.app/Contents/MacOS'
